e234b06abf901aaa959dc65652f581831b37ba0b
Nikenzsk6 <ajaniken307@gmail.com>
2025-09-17 17:08:56 +0700

    tambah atribut dan method

--- src/tim7_minuman/Mocha.java
+++ src/tim7_minuman/Mocha.java
@@ -9,5 +9,37 @@ package tim7_minuman;
  * @author Sukma Nur
  */
 public class Mocha {
+    String nama, jenis, ukuran, harga;
     
+    
+    public void setnama(String Nama){
+        this.nama=Nama;
+    }
+    public String getnama(){
+        return this.nama;
+    }
+    
+    
+    public void setjenis(String Jenis){
+        this.jenis=Jenis;
+    }
+    public String getjenis(){
+        return this.jenis;
+    }
+    
+    
+    public void setukuran(String Ukuran){
+        this.ukuran=Ukuran;
+    }
+    public String getukuran(){
+        return this.ukuran;
+    }
+    
+    
+    public void setharga(String Harga){
+        this.harga=Harga;
+    }
+    public String getharga(){
+        return this.harga;
+    }
 }
