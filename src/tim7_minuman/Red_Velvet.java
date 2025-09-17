66750fa6591743fc2fa2915a5298ab0aa89b0ffc
riyadlus78 <riyadlus2509.gmail.com>
2025-09-17 18:21:54 +0700

    segeer

--- src/tim7_minuman/Red_Velvet.java
+++ src/tim7_minuman/Red_Velvet.java
@@ -9,5 +9,31 @@ package tim7_minuman;
  * @author Sukma Nur
  */
 public class Red_Velvet {
+    String nama, harga, jenis, ukuran ;
+    
+    public void setnama(String namaMinuman){
+        this.nama = namaMinuman;
+    }
+    public void setharga(String hargaMinuman){
+        this.harga = hargaMinuman;
+    }
+    public void setjenis(String jenisMinuman){
+        this.jenis = jenisMinuman;
+    }
+    public void setukuran(String ukuranMinuman){
+        this.ukuran = ukuranMinuman;
+    }
+    public String  getnama(){
+        return this.nama;
+    }
+    public String  getharga(){
+        return this.harga;
+    }
+    public String  getjenis(){
+        return this.jenis;
+    }
+    public String  getukuran(){
+        return this.ukuran;
+    }
     
 }
