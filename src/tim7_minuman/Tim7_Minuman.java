14737096bc5f6846055a20d2fcd3c287111ee28f
SukmaNurSholehah <aja052670@gmail.com>
2025-09-17 14:26:30 +0700

    Versioning Git

---
+++ src/tim7_minuman/Tim7_Minuman.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tim7_minuman;
+
+/**
+ *
+ * @author Sukma Nur
+ */
+public class Tim7_Minuman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
