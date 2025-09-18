a3a5660510c93e411cc53fc49ee986ca31c55187
naila <nailabinti1@gmil.com>
2025-09-18 15:33:09 +0700

    tambah minuman taro

--- src/tim7_minuman/Taro.java
+++ src/tim7_minuman/Taro.java
@@ -9,5 +9,31 @@ package tim7_minuman;
  * @author Sukma Nur
  */
 public class Taro {
+     String Nama, Harga, Jenis, Ukuran;
     
+    public void setNama(String Namax){
+        this.Nama = Namax;
+    }
+    public void setHarga(String Hargax){
+        this.Harga = Hargax;
+    }
+    public void setJenis(String Jenisx){
+        this.Jenis = Jenisx;
+    }
+    public void setUkuran(String Ukuranx){
+        this.Ukuran = Ukuranx;
+    }
+    
+    public String getNama(){
+        return this.Nama;
+    }
+    public String getHarga(){
+        return  this.Harga;
+    }
+    public String getJenis(){
+        return this.Jenis;
+    }
+    public String getUkuran(){
+        return this.Ukuran;
+    }
 }
