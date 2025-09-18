0edffeb3c1c2084ed42c9a2ec964e15d2728812c
patimzaa10 <paatimzaaa@gmail.com>
2025-09-18 15:00:15 +0700

    vanillalatteza

--- src/tim7_minuman/Vanilla_Latte.java
+++ src/tim7_minuman/Vanilla_Latte.java
@@ -9,5 +9,39 @@ package tim7_minuman;
  * @author Sukma Nur
  */
 public class Vanilla_Latte {
+    String nama, harga, jenis, ukuran;
     
+    
+    public void setnama(String nama){
+        this.nama = nama;
+        
+    }
+    
+    public String getnama(){
+        return this.nama;
+    }
+    public void setharga(String harga ){
+        this.harga = harga;
+        
+    }
+    
+    public String getharga(){
+        return this.harga;
+    }
+    public void setjenis(String jenis){
+        this.jenis = jenis;
+        
+    }
+    
+    public String getjenis(){
+        return this.jenis;
+    }
+    public void setukuran(String ukuran){
+        this.ukuran = ukuran;
+        
+    }
+    
+    public String getukuran(){
+        return this.ukuran;
+    }
 }
