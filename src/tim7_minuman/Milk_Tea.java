d22d559133df1a318e124b8f89434ae187d26cd4
USER <USER@DESKTOP-1646F9I>
2025-09-19 09:31:48 +0700

    data minuman milk tea

--- src/tim7_minuman/Milk_Tea.java
+++ src/tim7_minuman/Milk_Tea.java
@@ -9,5 +9,31 @@ package tim7_minuman;
  * @author Sukma Nur
  */
 public class Milk_Tea {
+    String nama, harga, jenis, uuran;
+    
+    public void setnama(String nama44){
+        this.nama = nama44;
+    }
+    public String getnama(){
+        return this.nama;
+    }
+    public void setharga(String harga44){
+        this.harga = harga44;
+    }
+    public String getharga(){
+        return this.harga;
+    }
+    public void setjenis(String jenis44){
+        this.jenis = jenis44;
+    }
+    public String getjenis(){
+        return this.jenis;
+    }
+    public void setukuran(String ukuran44){
+        this.uuran = ukuran44;
+    }
+    public String getukuran(){
+        return this.uuran;
+    }
     
 }
