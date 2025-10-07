6eb39f6ecea3ced937e78b89dd9e8c8cee6ce97d
patimzaa10 <paatimzaaa@gmail.com>
2025-10-07 15:31:41 +0700

    tambah subclasslatte

---
+++ src/tim7_minuman/Milk_Tea.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tim7_minuman;
+
+/**
+ *
+ * @author Sukma Nur
+ */
+public class Milk_Tea {
+    String nama, harga, jenis, uuran;
+    
+    public void setnama(String nama44){
+        this.nama = nama44;
+    }
+    public String getnama(){
+        return this.nama;
+    }
+    public void setharga(String harga44){
+        this.harga = harga44;
+    }
+    public String getharga(){
+        return this.harga;
+    }
+    public void setjenis(String jenis44){
+        this.jenis = jenis44;
+    }
+    public String getjenis(){
+        return this.jenis;
+    }
+    public void setukuran(String ukuran44){
+        this.uuran = ukuran44;
+    }
+    public String getukuran(){
+        return this.uuran;
+    }
+    
+}
