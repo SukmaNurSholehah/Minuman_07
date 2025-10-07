19425049affb387600949df30ec34b41a02a4a42
naila <nailabinti1@gmil.com>
2025-10-07 15:31:37 +0700

    tambah subclas taro

---
+++ src/tim7_minuman/Vanilla_Latte.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tim7_minuman;
+
+/**
+ *
+ * @author Sukma Nur
+ */
+public class Vanilla_Latte {
+    String nama, harga, jenis, ukuran;
+     
+    public void setnama(String nama){
+        this.nama = nama;    
+    } 
+    public String getnama(){
+        return this.nama;
+    }
+    
+    public void setharga(String harga ){
+        this.harga = harga;   
+    }
+    public String getharga(){
+        return this.harga;
+    }
+    
+    public void setjenis(String jenis){
+        this.jenis = jenis;   
+    }
+    public String getjenis(){
+        return this.jenis;
+    }
+    
+    public void setukuran(String ukuran){
+        this.ukuran = ukuran;   
+    }
+    public String getukuran(){
+        return this.ukuran;
+    }
+}
